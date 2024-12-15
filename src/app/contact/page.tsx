9c624a0712913eee5afc3687db75907d92ed4474
ecblandy <Viny19381@hotmail.com>
2024-12-15 13:23:52 -0300

    Feat: terminando layout página de contato

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,85 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "71 99402-7893",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "devblandy@hotmail.com",
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Addrss",
+    description: "Salvador - BA",
+  },
+];
+
 export default function Contact() {
-  return <>Contact page</>;
+  return (
+    <motion.section
+      initial={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6 "
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Lets work togheter</h3>
+              <p className="text-white/60 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                facilis!
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Nome" />
+                <Input type="lastname" placeholder="Sobrenome" />
+                <Input type="email" placeholder="Email" />
+                <Input type="phone" placeholder="Telefone" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="select a service" />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Developer</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 }
