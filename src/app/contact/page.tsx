96b814dd6a82fc932d96117f8778fb670e8a88fd
ecblandy <Viny19381@hotmail.com>
2024-12-15 13:49:53 -0300

    Feat: terminando layout página de contato

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,7 +17,7 @@ import { motion } from "framer-motion";
 const info = [
   {
     icon: <FaPhoneAlt />,
-    title: "Phone",
+    title: "Telefone",
     description: "71 99402-7893",
   },
   {
@@ -27,7 +27,7 @@ const info = [
   },
   {
     icon: <FaMapMarkedAlt />,
-    title: "Addrss",
+    title: "Endereço",
     description: "Salvador - BA",
   },
 ];
@@ -45,7 +45,7 @@ export default function Contact() {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Lets work togheter</h3>
               <p className="text-white/60 ">
@@ -72,11 +72,34 @@ export default function Contact() {
                   </SelectGroup>
                 </SelectContent>
               </Select>
+              {/* textarea */}
+              <Textarea
+                className="h-[200px]"
+                placeholder="Escreva sua mensagem aqui."
+              />
+              {/* btn */}
+              <Button size="md" className="max-w-40 rounded">
+                Enviar mensagem
+              </Button>
             </form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6 ">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
